127ab92a2f8020c5cda282be8d48a3bb13290410
traceimp <traceimp@outlook.kr>
2019-01-22 23:50:54 +0900

    2장 > 예제 1

---
+++ 2장/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BrainCSharp
+{
+    class HelloWorld
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용방법: HelloWorld.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hellom {0}!", args[0]);
+        }
+    }
+}
