58cc97aaca89587ceae525b9b5ef80090f63b2a4
traceimp <traceimp@outlook.kr>
2019-03-27 17:41:51 +0900

    14장 완료

---
+++ 14장/14_2/14_2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _14_2
+{
+    class Program
+    {
+        delegate int Result(int a);
+        delegate void Result2(int[] arr);
+
+        static void Main(string[] args)
+        {
+            // 문제 1
+            //Func<int> func_1 = () => 10;
+            //Func<int, int> func_2 = (a) => a * 2;
+
+            //Console.WriteLine(func_1() + func_2(30));
+
+            //문제 2
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            //foreach (int a in array)
+            //{
+            //    Action action = new Action
+            //        (
+            //            delegate ()
+            //            {
+            //                Console.WriteLine(a * a);
+            //            }
+            //        );
+            //    action.Invoke();
+            //}
+
+            // 기본 람다식
+            Console.WriteLine(">> 기본 람다식 <<");
+            Result re = (int a) => a * a;
+            foreach (int a in array)
+                Console.WriteLine(re(a));
+            Console.WriteLine();
+
+            // 문 형식 람다식
+            Console.WriteLine(">> 문형식 람다식 <<");
+            Result2 re2 = (int[] arr) =>
+            {
+                foreach (int a in arr)
+                    Console.WriteLine(a * a);
+            };
+            re2(array);
+            Console.WriteLine();
+
+            // Func 람다식
+            Console.WriteLine(">> Func 람다식 <<");
+            Func<int, int> func = (x) => x * x;
+            foreach (int a in array)
+                Console.WriteLine(func(a));
+            Console.WriteLine();
+
+            // Action 람다식
+            Console.WriteLine(">> Action 람다식 <<");
+            Action<int[]> action = (int[] arr) =>
+            {
+                foreach (int a in arr)
+                    Console.WriteLine(a * a);
+            };
+            action(array);
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ 14장/ExpressionBodiedMember/ExpressionBodiedMember/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpressionBodiedMember
+{
+    class FriendList
+    {
+        private List<string> list = new List<string>();
+
+        public void Add(string name) => list.Add(name);         // 메소드를 식으로 1
+        public void Remove(string name) => list.Remove(name);   // 메소드를 식으로 2
+        public void PrintAll()
+        {
+            foreach (var s in list)
+                Console.WriteLine(s);
+        }
+
+        public FriendList() => Console.WriteLine("FriendList()");   // 생성자를 식으로
+        ~FriendList() => Console.WriteLine("~FriendList()");        // 종료자를 식으로
+
+        // public int Capacity => list.Capacity;    // 읽기 전용
+        public int Capacity // 속성
+        {
+            get => list.Capacity;
+            set => list.Capacity = value;
+        }
+
+        // public string this[int index] => list[index];    // 읽기 전용
+        public string this[int index]   // 인덱서
+        {
+            get => list[index];
+            set => list[index] = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FriendList obj = new FriendList();
+            obj.Add("Eeny");
+            obj.Add("Meeny");
+            obj.Add("Miny");
+            obj.Remove("Eeny");
+            obj.PrintAll();
+
+            Console.WriteLine($"{obj.Capacity}");
+            obj.Capacity = 10;
+            Console.WriteLine($"{obj.Capacity}");
+
+            Console.WriteLine($"{obj[0]}");
+            obj[0] = "Moe";
+            obj.PrintAll();
+        }
+    }
+}
