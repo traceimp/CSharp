1499901a59c366ad9acaf35d9fb106815a3cb65c
traceimp <traceimp@outlook.kr>
2019-03-31 22:59:36 +0900

    15장 완료

---
+++ 15장/Ex15_1/Ex15_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex15_1
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car(){Cost=56,MaxSpeed=120},
+                new Car(){Cost=70,MaxSpeed=158},
+                new Car(){Cost=45,MaxSpeed=180},
+                new Car(){Cost=32,MaxSpeed=200},
+                new Car(){Cost=82,MaxSpeed=280}
+            };
+
+            // where 절로만 검색
+            var selected = from car in cars
+                           where car.Cost > 50
+                           where car.MaxSpeed > 150
+                           select car;
+
+            foreach (var c in selected)
+                Console.WriteLine($"Cost: {c.Cost}, Maxspeed: {c.MaxSpeed}");
+        }
+    }
+}
---
+++ 15장/Ex15_2/Ex15_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex15_2
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car(){Cost=56,MaxSpeed=120},
+                new Car(){Cost=70,MaxSpeed=158},
+                new Car(){Cost=45,MaxSpeed=180},
+                new Car(){Cost=32,MaxSpeed=200},
+                new Car(){Cost=82,MaxSpeed=280}
+            };
+
+            //var selected = cars.Where(c => c.Cost < 60).OrderBy(c => c.Cost);
+            var selected = from car in cars
+                           where car.Cost < 60
+                           orderby car.Cost
+                           select car;
+
+            foreach (var c in selected)
+                Console.WriteLine($"Cost: {c.Cost}, Maxspeed: {c.MaxSpeed}");
+        }
+    }
+}
